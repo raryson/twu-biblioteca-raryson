8a956272e67268e9fafd3ec6f38b8786a2b67e1a
Raryson Pereira Rost <raryson.rost@gmail.com>
2019-01-31 11:21:30 -0200

    Created Hello message test and implementation

---
+++ src/com/twu/biblioteca/Messages.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca;
+
+public class Messages {
+    public static String HelloMessage(){
+        return "Welcome to Biblioteca." +
+            " Your one-stop-shop for great book title in Bangalore!";
+    }
+}
---
+++ test/com/twu/biblioteca/HelloTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloTest {
+
+    @Test
+    public void WhenUserEnterInApplication() {
+        final String expectedHelloMessage = "Welcome to Biblioteca." +
+                " Your one-stop-shop for great book title in Bangalore!";
+        assertEquals(Messages.HelloMessage(), expectedHelloMessage);
+    }
+}
